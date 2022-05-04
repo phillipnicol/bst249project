d854f407d1cf8e4c2cedc7efdb369f14bff5c037
phillipnicol <43178367+phillipnicol@users.noreply.github.com>
2022-05-04 17:25:44 -0400

    Change

--- R/blasso.R
+++ R/blasso.R
@@ -9,18 +9,13 @@ Blasso <- function(y,X,iters=2000,warmup=1000) {
   results$beta <- matrix(0,nrow=iters-warmup,ncol=p)
   results$sigma2 <- rep(0,iters-warmup)
   results$lambda <- rep(0,iters-warmup)
-  results$tau2 <- rep(0,iters-warmup)
 
-  #Initialize beta using ridge
-  beta <- solve((X%*%t(X)+diag(0.1,nrow=p)))%*%t(X)%*%y
-  #beta <- rnorm(n=p)
-  residuals <- sum((y-X%*%beta)^2)
-  sigma2 <- residuals/n
-
-
-  lambda <- p*sqrt(sigma2)/(sum(abs(beta)))
-
-  tau2 <- rexp(n=1,rate=lambda^2)
+  lambda = lambda_init(y,X)
+  tau2 = rexp(p,rate = lambda^2)
+  #beta = as.vector(lm(y~X-1)$coefficients)
+  #beta = rep(0,p)
+  beta = as.vector(coef(glmnet(X,y, intercept =FALSE),s=0.1))[2:(p+1)]
+  sigma2 <- 1
 
   for(i in 1:iters) {
     if(i < warmup) {
@@ -39,7 +34,6 @@ Blasso <- function(y,X,iters=2000,warmup=1000) {
       results$beta[i-warmup,] <- beta
       results$sigma2[i-warmup] <- sigma2
       results$lambda[i-warmup] <- lambda
-      results$tau2[i-warmup] <- tau2
     }
   }
 
@@ -62,39 +56,47 @@ Blasso <- function(y,X,iters=2000,warmup=1000) {
 bayes_lasso_emp_bayes <- function(y,X,beta,sigma2,tau2,lambda){
   n = dim(X)[1]
   p = dim(X)[2]
-  D_inv = diag(1/tau2,nrow=p)
+  D_inv = diag(1/tau2)
   A = t(X)%*%X+D_inv
   A_inv = solve(A)
+  #A_inv = diag(tau2)
   mean = A_inv%*%t(X)%*%y
   var = sigma2*A_inv
-  bet_new = mvrnorm(1,mean,var)
+  #bet_new = mvrnorm(1,mean,var)
+  bet_new = t(rmvnorm(1,mean,var))
   err = y-X%*%bet_new
   scale_new = t(err)%*%err/2+t(bet_new)%*%D_inv%*%bet_new/2
-  sig_new = rinvgamma(1,(n-1)/2+(p/2),scale=scale_new)
+  sig_new = invgamma::rinvgamma(1,(n-1)/2+(p/2),scale=1/scale_new)
   lamb_tau = lambda^2
   tau_new = rep(NA, length(tau2))
   for(i in 1:length(tau2)){
     mu_tau = sqrt(lamb_tau*sig_new/(bet_new[i]^2))
     a = rinvgauss(1,mean=mu_tau,shape=lamb_tau)
-    tau_new[i] = 1/a^2
+    tau_new[i] = 1/a
   }
-  lamb_new = sqrt((2*p)/mean(tau_new))
-  val <- list()
-  val$beta <- bet_new
-  val$sigma2 <- sig_new
-  val$tau2 <- tau_new
-  val$lambda <- lamb_new
-  return(val)
+  lamb_new = sqrt((2*p)/sum(tau_new))
+  return(list(beta = bet_new,sigma2 = sig_new,tau2 = tau_new,lambda = lamb_new))
 }
 
+
+
 lambda_init <- function(y,X){
+  n = dim(X)[1]
   p = dim(X)[2]
-  lm = lm(y~X)
-  beta = lm$coefficients
-  sd = sd(lm$residuals)
-  lambda = p*sd/sum(abs(beta$residuals))
+  beta = coef(glmnet(X,y),s=0.1)
+  pred = predict(glmnet(X,y),X,s=0.1)
+  sd = sd(y-pred)
+  lambda = p*sd/sum(abs(beta))
+  if(n>p){
+    lm = lm(y~X)
+    beta = lm$coefficients
+    sd = sd(lm$residuals)
+    lambda = p*sd/sum(abs(beta))
+  }
   return(lambda)
 }
+
+
 
 ### Instead, if we are using a Gamma hyperprior on lambda^2, with shape parameter r and scale parameter delta, we have the following Gibbs updation step
 bayes_lasso_hyp_prior <- function(y,X,beta,sigma2,tau2,lambda,r,delta){
@@ -108,7 +110,8 @@ bayes_lasso_hyp_prior <- function(y,X,beta,sigma2,tau2,lambda,r,delta){
   bet_new = mvrnorm(1,mean,var)
   err = y-X%*%bet_new
   scale_new = t(err)%*%err/2+t(bet_new)%*%D_inv%*%bet_new/2
-  sig_new = rinvgamma(1,(n-1)/2+(p/2),scale=scale_new)
+  #sig_new = rinvgamma(1,(n-1)/2+(p/2),scale=scale_new)
+  sig_new = 1
   lamb_tau = lambda^2
   tau_new = rep(NA, length(tau2))
   for(i in 1:length(tau2)){
--- R/simulate.R
+++ R/simulate.R
@@ -4,7 +4,7 @@
 #' @export
 simulateTest <- function(n,p,p1,beta1,phi.nlp,phi.normal) {
   #Number of methods x number of metrics
-  results <- matrix(nrow=3,ncol=3)
+  results <- matrix(0,nrow=3,ncol=4)
 
   beta <- rep(0,p)
   beta[1:p1] <- sample(c(-1,1),size=p1,replace=TRUE)*beta1
@@ -18,6 +18,7 @@ simulateTest <- function(n,p,p1,beta1,phi.nlp,phi.normal) {
 
   out.nlp <- SpSlNLP(y=y,X=X,phi=phi.nlp,warmup=50,iters=100)
   out.norm <- SpSlNormal(y=y,X=X,phi=phi.normal,warmup=50,iters=100)
+  out.bl <- Blasso(y=y,X=X,warmup=100,iters=200)
 
   #Analyze out.nlp
   pm_beta <- colMeans(out.nlp$beta)
@@ -26,7 +27,9 @@ simulateTest <- function(n,p,p1,beta1,phi.nlp,phi.normal) {
   pip <- colMeans(out.nlp$z)
 
   results[1,1] <- rmse; results[1,2] <- angle
-  results[1,3] <- mean(apply(out.nlp$z,1,function(x) mcc(preds=ifelse(x>0,1,0),actual=ifelse(beta>0,1,0))))
+  results[1,3] <- mean(apply(out.nlp$z,1,function(x) mcc(preds=ifelse(x>0,1,0),actual=ifelse(beta!=0,1,0))))
+  results[1,4] <- 0 #FN RATE
+
 
   #Analyze out.norm
   pm_beta <- colMeans(out.norm$beta)
@@ -35,10 +38,155 @@ simulateTest <- function(n,p,p1,beta1,phi.nlp,phi.normal) {
   pip <- colMeans(out.norm$z)
 
   results[2,1] <- rmse; results[2,2] <- angle
-  results[2,3] <-  mean(apply(out.norm$z,1,function(x) mcc(preds=ifelse(x>0,1,0),actual=ifelse(beta>0,1,0))))
+  results[2,3] <-  mean(apply(out.norm$z,1,function(x) mcc(preds=ifelse(x>0,1,0),actual=ifelse(beta!=0,1,0))))
+  results[2,4] <- 0 #FN RATE
 
+
+  # Analyze out.bl
+  pm_beta <- colMeans(out.bl$beta)
+  rmse <- sqrt(mean((pm_beta - beta)^2))
+  angle <- sum(pm_beta*beta)/(sqrt(sum(pm_beta^2)*sum(beta^2)))
+  z <- apply(out.bl$beta,2,function(x) {
+    lb <- quantile(x,0.025)
+    ub <- quantile(x,0.975)
+    if(0 < ub & 0 > lb) {
+      0
+    } else {
+      1
+    }
+  })
+  mcc <- mcc(preds=z,actual=ifelse(beta != 0,1,0))
 
   return(results)
+}
+
+
+## First simulation study with uncorrelated covariates
+FullSimulate1 <- function(reps,n,p,phi.nlp,phi.normal) {
+  #Low dimensional setting
+  #N and P
+
+  #Number of non-nulls
+  p1 <- c(10,20,30)
+  nalg <- 3
+
+  data <- matrix(0,nrow=0,ncol=5)
+  for(i in 1:length(p1)) {
+    mcc <- matrix(0,nrow=nalg,ncol=reps)
+    fn <- matrix(0,nrow=nalg,ncol=reps)
+    rmse <- matrix(0,nrow=nalg,ncol=reps)
+    angle <- matrix(0,nrow=nalg,ncol=reps)
+    for(j in 1:reps) {
+      results <- simulateTest(n,p,p1[i],beta1,phi.nlp,phi.normal)
+      rmse[,j] <- results[,1]
+      angle[,j] <- results[,2]
+      mcc[,j] <- results[,3]
+      fn[,j] <- results[,4]
+    }
+
+    data <- rbind(data,c(rowMeans(rmse)[1],"SpSl-Normal",
+                         p1[i],sd=sd(rmse[1,]),"RMSE"))
+    data <- rbind(data,c(rowMeans(angle)[1],"SpSl-Normal",
+                         p1[i],sd=sd(angle[1,]),"Cosine"))
+    data <- rbind(data,c(rowMeans(mcc)[1],"SpSl-Normal",
+                         p1[i],sd=sd(mcc[1,]),"MCC"))
+    data <- rbind(data,c(rowMeans(fn)[1],"SpSl-Normal",
+                         p1[i],sd=sd(fn[1,]),"FN"))
+
+    data <- rbind(data,c(rowMeans(rmse)[2],"SpSl-NLP",
+                         p1[i],sd=sd(rmse[2,]),"RMSE"))
+    data <- rbind(data,c(rowMeans(angle)[2],"SpSl-NLP",
+                         p1[i],sd=sd(angle[2,]),"Cosine"))
+    data <- rbind(data,c(rowMeans(mcc)[2],"SpSl-NLP",
+                         p1[i],sd=sd(mcc[2,]),"MCC"))
+    data <- rbind(data,c(rowMeans(fn)[2],"SpSl-NLP",
+                         p1[i],sd=sd(fn[2,]),"FN"))
+
+    data <- rbind(data,c(rowMeans(rmse)[3],"BL",
+                         p1[i],sd=sd(rmse[3,]),"RMSE"))
+    data <- rbind(data,c(rowMeans(angle)[3],"BL",
+                         p1[i],sd=sd(angle[3,]),"Cosine"))
+    data <- rbind(data,c(rowMeans(mcc)[3],"BL",
+                         p1[i],sd=sd(mcc[3,]),"MCC"))
+    data <- rbind(data,c(rowMeans(fn)[3],"BL",
+                         p1[i],sd=sd(fn[3,]),"FN"))
+  }
+
+  data.LD <- data
+
+  ## High dimensional setting Switch n and p
+  n.new <- p
+  p <- n
+  n <- n.new
+
+  data <- matrix(0,nrow=0,ncol=5)
+  for(i in 1:length(p1)) {
+    mcc <- matrix(0,nrow=nalg,ncol=reps)
+    fn <- matrix(0,nrow=nalg,ncol=reps)
+    rmse <- matrix(0,nrow=nalg,ncol=reps)
+    angle <- matrix(0,nrow=nalg,ncol=reps)
+    for(j in 1:reps) {
+      results <- simulateTest(n,p,p1[i],beta1,phi.nlp,phi.normal)
+      rmse[,j] <- results[,1]
+      angle[,j] <- results[,2]
+      mcc[,j] <- results[,3]
+      fn[,j] <- results[,4]
+    }
+
+    data <- rbind(data,c(rowMeans(rmse)[1],"SpSl-Normal",
+                         p1[i],sd=sd(rmse[1,]),"RMSE"))
+    data <- rbind(data,c(rowMeans(angle)[1],"SpSl-Normal",
+                         p1[i],sd=sd(angle[1,]),"Cosine"))
+    data <- rbind(data,c(rowMeans(mcc)[1],"SpSl-Normal",
+                         p1[i],sd=sd(mcc[1,]),"MCC"))
+    data <- rbind(data,c(rowMeans(fn)[1],"SpSl-Normal",
+                         p1[i],sd=sd(fn[1,]),"FN"))
+
+    data <- rbind(data,c(rowMeans(rmse)[2],"SpSl-NLP",
+                         p1[i],sd=sd(rmse[2,]),"RMSE"))
+    data <- rbind(data,c(rowMeans(angle)[2],"SpSl-NLP",
+                         p1[i],sd=sd(angle[2,]),"Cosine"))
+    data <- rbind(data,c(rowMeans(mcc)[2],"SpSl-NLP",
+                         p1[i],sd=sd(mcc[2,]),"MCC"))
+    data <- rbind(data,c(rowMeans(fn)[2],"SpSl-NLP",
+                         p1[i],sd=sd(fn[2,]),"FN"))
+
+    data <- rbind(data,c(rowMeans(rmse)[3],"BL",
+                         p1[i],sd=sd(rmse[3,]),"RMSE"))
+    data <- rbind(data,c(rowMeans(angle)[3],"BL",
+                         p1[i],sd=sd(angle[3,]),"Cosine"))
+    data <- rbind(data,c(rowMeans(mcc)[3],"BL",
+                         p1[i],sd=sd(mcc[3,]),"MCC"))
+    data <- rbind(data,c(rowMeans(fn)[3],"BL",
+                         p1[i],sd=sd(fn[3,]),"FN"))
+  }
+
+  data.LD <- cbind(data.LD, "n=100, p=50")
+  data <- cbind(data, "n=50, p=100")
+  data <- rbind(data.LD,data)
+
+
+}
+
+
+
+makePlot <- function(data) {
+  data<-data.frame(metric=as.numeric(data[,1]),
+                  Algorithm=data[,2],
+                  S=as.numeric(data[,3]),
+                  sd=as.numeric(data[,4]),
+                  type=data[,5],
+                  dimension=data[,6])
+
+  p <- ggplot(data=data,aes(x=S,y=metric,color=Algorithm,
+                            ymin=metric-sd,ymax=metric+sd))
+  p <- p + geom_point()
+  p <- p + geom_line()
+  p <- p + geom_errorbar(width=0.1)
+  p <- p + theme_linedraw()
+  p <- p + xlab("# of Non-Nulls")
+  p <- p + ylab("")
+  p <- p + facet_grid(type ~ dimension)
 }
 
 
